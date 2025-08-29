7d3753896f00903ee775caef6cb5b84c974361e1
maiconnDutra <unifacul0@gmail.com>
2025-08-29 20:43:24 -0300

    Criação do solution e os projetos

---
+++ AlunoSolution/Aluno.Dominio/Aluno.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Aluno.Dominio
+{
+    public class Aluno
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Idade { get; set; }
+    }
+}
